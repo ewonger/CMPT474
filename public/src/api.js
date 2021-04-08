62c26b4b9c2e3a110ae38fa7bf4afd7c43704233
Eric Wong <erichwlwong@gmail.com>
2021-04-08 00:57:01 -0700

    add / to api gateway url

---
+++ public/src/api.js
@@ -0,0 +1,8 @@
+let api = {
+    gateway: " https://purbbcv5e1.execute-api.us-east-1.amazonaws.com/dev/",
+    albumBucketName: "petfindr-1",
+    bucketRegion: "us-east-1",
+    IdentityPoolId: "us-east-1:d84dbfa1-3977-419b-904d-23dcb645619d",
+}
+
+export default api;
